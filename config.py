74fdba9ba4d2450d67d48456cd393dc39da1a55e
hgs1217 <mf20080540@126.com>
2018-06-19 11:12:13 +0800

    add more to .ignore

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 # @Author:      HgS_1217_
 # @Create Date: 2018/6/19
 
+CKPT_PREFIX = "D:/Computer Science/Github/Gene-Chip-Data/model/"
+CKPT_PATH = CKPT_PREFIX + "model.ckpt"
+LOG_PATH = "D:/Computer Science/Github/Protein-Subcellular-Location/log"
+
+DATASET_PATH = "D:/Computer Science/dataset/Gene_Chip_Data"
